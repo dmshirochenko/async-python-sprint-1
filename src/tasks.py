a77155493fbaf2279ccf9bcca27b2fc6a132d93b
Dmitriy Shirochenko <d.shirochenko@criteo.com>
2023-12-10 22:12:25 +0100

    type hinting for DataFetchTask has been added

--- src/tasks.py
+++ src/tasks.py
@@ -1,20 +1,23 @@
 import logging
 import concurrent.futures
+from typing import Callable, Dict, Any
 
 logger = logging.getLogger(__name__)
 
 
 class DataFetchingTask:
-    def __init__(self, url_dict, fetch_func, max_workers=5):
-        self.url_dict = url_dict
-        self.fetch_func = fetch_func
-        self.max_workers = max_workers
+    def __init__(self, url_dict: Dict[str, str], fetch_func: Callable[[str], Any], max_workers: int = 5) -> None:
+        self.url_dict: Dict[str, str] = url_dict
+        self.fetch_func: Callable[[str], Any] = fetch_func
+        self.max_workers: int = max_workers
 
-    def fetch_all(self):
+    def fetch_all(self) -> Dict[str, Any]:
         with concurrent.futures.ThreadPoolExecutor(max_workers=self.max_workers) as executor:
-            future_to_city = {executor.submit(self.fetch_func, url): city for city, url in self.url_dict.items()}
+            future_to_city: Dict[concurrent.futures.Future, str] = {
+                executor.submit(self.fetch_func, url): city for city, url in self.url_dict.items()
+            }
 
-            results = {}
+            results: Dict[str, Any] = {}
             for future in concurrent.futures.as_completed(future_to_city):
                 city = future_to_city[future]
                 try:
