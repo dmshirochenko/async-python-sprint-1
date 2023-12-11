224fe4a472a02cb2403dbd336cb2b79c5060ae7d
Dmitriy Shirochenko <d.shirochenko@criteo.com>
2023-12-12 00:39:35 +0100

    task 2 - data calculation has been added

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import logging.config
+import multiprocessing as mp
 
 from config.logger import LOGGING
 from src.utils import CITIES
 from external.client import YandexWeatherAPI
-from src.tasks import DataFetchingTask
+from external.analyzer import analyze_json
+from src.tasks import DataFetchingTask, DataCalculationTask
 
 
 logging.config.dictConfig(LOGGING)
@@ -17,7 +19,12 @@ def check_python_version():
 
 
 if __name__ == "__main__":
+    mp.set_start_method('fork')
     logger.info("Fetching data ...")
     check_python_version()
     data_fetching_app_instacnce = DataFetchingTask(CITIES, YandexWeatherAPI.get_forecasting)
     fetched_data = data_fetching_app_instacnce.fetch_all()
+    logger.info(f'Fetched data = {fetched_data}')
+    data_calculation_instance = DataCalculationTask(fetched_data, analyze_json)
+    analyzed_data = data_calculation_instance.calculate()
+    logger.info(f'Analyzed data = {analyzed_data}')
--- src/tasks.py
+++ src/tasks.py
@@ -1,6 +1,8 @@
 import logging
 import concurrent.futures
+from multiprocessing import Process, Queue
 from typing import Callable, Dict, Any
+
 
 logger = logging.getLogger(__name__)
 
@@ -25,11 +27,57 @@ class DataFetchingTask:
                     results[city] = data
                 except Exception as exc:
                     logger.error(f"Fetching data for {city} generated an exception: {exc}")
+
             return results
 
 
+class Worker(Process):
+    def __init__(self, func: Callable, task_queue: Queue, result_queue: Queue):
+        super().__init__()
+        self.func = func
+        self.task_queue = task_queue
+        self.result_queue = result_queue
+
+    def run(self):
+        while True:
+            key, value = self.task_queue.get()
+            if key is None:
+                break
+            result = self.func(value)
+            self.result_queue.put((key, result))
+
+
 class DataCalculationTask:
-    pass
+    def __init__(self, data_dict: Dict[Any, Any], calc_func: Callable[[Any], Any], num_workers: int = 4):
+        self.data_dict = data_dict
+        self.calc_func = calc_func
+        self.num_workers = num_workers
+
+    def calculate(self) -> Dict[Any, Any]:
+        task_queue = Queue()
+        result_queue = Queue()
+        workers = []
+
+        for _ in range(self.num_workers):
+            worker = Worker(self.calc_func, task_queue, result_queue)
+            workers.append(worker)
+            worker.start()
+
+        for key, value in self.data_dict.items():
+            task_queue.put((key, value))
+
+        for _ in range(self.num_workers):
+            task_queue.put((None, None))
+
+        results = {}
+        for _ in range(len(self.data_dict)):
+            key, result = result_queue.get()
+            results[key] = result
+
+        for worker in workers:
+            worker.join()
+
+        return results
 
 
 class DataAggregationTask:
